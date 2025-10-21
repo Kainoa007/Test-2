9bf671dbbb987671029948f7af5eac8e88d2a36e
eparra-raymond-code <e.parra-raymond@rubika-edu.ca>
2025-10-20 20:48:50 -0400

    muejejejeje

---
+++ Assets/Script/InteractionDetector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionDetector : MonoBehaviour
+{
+    private IInteractable interactableInRange = null; //closest interactable 
+    public GameObject interactionIcon;
+   
+    void Start()
+    {
+        interactionIcon.SetActive(false);
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if(context.performed && interactableInRange != null)
+        {
+            interactableInRange.interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.TryGetComponent(out IInteractable interactable) && interactable.CanInteract())
+        {
+            interactableInRange = interactable;
+            interactionIcon.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable == interactableInRange)
+        {
+            interactableInRange = null;
+            interactionIcon.SetActive(false);
+        }
+    }
+}
--- Assets/Script/NpcDialogue.cs
+++ Assets/Script/NpcDialogue.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NpcDialogue", menuName = "NPC Dialogue")]
 public class NpcDialogue : ScriptableObject
 {
-    public string npcName;  // Name of the NPC
-    public Sprite npcPortrait;  // Portrait image of the NPC
-    public string[] dialogueLines;  // Array of dialogue lines
-    public  float typingSpeed = 0.05f; // Time delay between each character
-    public AudioClip voicesound; // Sound played when a character is typed 
-    public float voicePitch = 1f; // Pitch of the voice sound
-    public bool[] autoProgressingLines; // Array indicating if each line auto progresses
-    public float autoProgressDelay = 2f; // Delay before auto progressing to the next line
+    public string npcName;  
+    public Sprite npcPortrait;  
+    public string[] dialogueLines;
+    public bool[] autoProgressingLines;
+    public float autoProgressDelay = 1.5f;
+    public  float typingSpeed = 0.05f; 
+    public AudioClip voicesound; 
+    public float voicePitch = 1f;
+    internal object autoProgressLines;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
