9d9911a3d603a4216211243ec8f7b3ab2f1e8e4d
Kainoa007 <pomarekainoa@gmail.com>
2025-10-23 15:41:54 -0400

    small change
    yea

--- Assets/Script/Load_Specific_Scene.cs
+++ Assets/Script/Load_Specific_Scene.cs
@@ -3,11 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Load_Specific_Scene : MonoBehaviour
 {
+    public string sceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level01");
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
